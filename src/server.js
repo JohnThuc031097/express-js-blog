a5d8f83917e5036c5fb8ab001bbb07bc40f26546
JohnThuc1997 <johnthuc1997@gmail.com>
2021-05-20 05:26:18 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import handlebars from "express-handlebars";
 import morgan from "morgan";
 import sass from "node-sass";
@@ -9,32 +9,34 @@ import route from "./routes/index.route.js";
 
 const app = express();
 
-const host = 'localhost';
+const host = "localhost";
 const port = 3000;
-const __dirname = path.join(path.resolve(), 'src/');
+const __dirname = path.join(path.resolve(), "src/");
 const appFolder = {
-  resources: path.join(__dirname, '/resources'),
-  public: path.join(__dirname, '/public')
+    resources: path.join(__dirname, "/resources"),
+    public: path.join(__dirname, "/public"),
 };
 
 // Set Folder Static
 app.use(express.static(appFolder.public));
 // HTTP logger
-  app.use(morgan('dev'));
+app.use(morgan("dev"));
 // Midleware
-  app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 // Template Handlebars
-app.engine('hbs', handlebars({ extname: '.hbs' }));
-app.set('view engine', 'hbs');
-app.set('views', path.join(appFolder.resources, '/views'));
+app.engine("hbs", handlebars({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(appFolder.resources, "/views"));
 
 // HTTP Protocol
 route(app);
 
 app.listen(port, () => {
-  console.log(`App listening at http://${host}:${port}`);
+    console.log(`App listening at http://${host}:${port}`);
 });
